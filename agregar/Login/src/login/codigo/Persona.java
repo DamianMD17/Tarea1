385cf728dc6bde87a7b5b09f86b31edc1b7b4a77
DamianMD17 <147353544+DamianMD17@users.noreply.github.com>
2023-10-26 23:12:46 -0600

    Muestra correcto o incorrecto

---
+++ agregar/Login/src/login/codigo/Persona.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package login.codigo;
+
+/**
+ *
+ * @author damia
+ */
+public class Persona {
+
+    private String nombre;
+    private String contra;
+    private String carnet;
+
+    public Persona(String nombre, String contra, String carnet) {
+        this.nombre = nombre;
+        this.contra = contra;
+        this.carnet = carnet;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+}
